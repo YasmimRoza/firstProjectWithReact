97eb8ef809175cf5b8a1b3ce945fbc739bb93d21
YasmimRoza <yasmim.souza@cits.br>
2022-08-01 17:46:45 -0400

    feat: realizado a conclusão do reuso do campo de toas no menu lateral

--- metrialui-react-type/src/shared/components/menu-lateral/MenuLateral.tsx
+++ metrialui-react-type/src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,16 +1,46 @@
 import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from '@mui/material';
 import { Box } from '@mui/system';
+import { useMatch, useNavigate, useResolvedPath } from 'react-router-dom';
 import { useDrawerContext } from '../../contexts';
 
 interface IDrawerProviderProps {
 	children?: React.ReactNode
 }
 
+interface IListItemLinkProps {
+  to: string;
+  icon: string;
+  label: string;
+  onClick: (() => void) | undefined; 
+}
+
+const ListItemLink: React.FC<IListItemLinkProps> = ({ to, icon, label, onClick }) => {
+
+  const navigate = useNavigate();
+
+  const resolvedPath = useResolvedPath(to);
+  const match = useMatch({ path: resolvedPath.pathname, end: false});
+
+  const handleClick = () => {
+    navigate(to);
+    onClick?.(); // Se for verdadeiro execute se não for execute mesmo assim
+  };
+
+  return (
+    <ListItemButton selected={!!match} onClick={handleClick}>
+      <ListItemIcon>
+        <Icon>{icon}</Icon>
+      </ListItemIcon>
+      <ListItemText primary={label}/>
+    </ListItemButton>
+  );
+}; 
+
 export const MenuLateral: React.FC<IDrawerProviderProps> = ({ children }) => {
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down('sm'));
 
-  const { isDrawerOpen, toggleDrawerOpen } = useDrawerContext();
+  const { isDrawerOpen, toggleDrawerOpen, drawerOptions } = useDrawerContext();
 
   return (
     <>
@@ -28,12 +58,15 @@ export const MenuLateral: React.FC<IDrawerProviderProps> = ({ children }) => {
 
           <Box>
             <List component="nav">
-              <ListItemButton>
-                <ListItemIcon>
-                  <Icon>home</Icon>
-                </ListItemIcon>
-                <ListItemText primary="Página Inicial"/>
-              </ListItemButton>
+              {drawerOptions.map(drawerOptions => (
+                <ListItemLink
+                  key={drawerOptions.path}
+                  to={drawerOptions.path}
+                  icon={drawerOptions.icon}
+                  label={drawerOptions.label}
+                  onClick={smDown ? toggleDrawerOpen : undefined}
+                />
+              ))}
             </List>
           </Box>
 
--- metrialui-react-type/src/shared/contexts/DrawerContext.tsx
+++ metrialui-react-type/src/shared/contexts/DrawerContext.tsx
@@ -1,9 +1,16 @@
-/* eslint-disable react/prop-types */
 import { createContext, useCallback, useContext, useState } from 'react';
+
+interface IDrawerOption {
+  icon: string;
+  path: string;
+  label: string;
+}
 
 interface IDrawerContextData {
     isDrawerOpen: boolean;
+    drawerOptions: IDrawerOption[];
     toggleDrawerOpen: () => void;
+    setDrawerOptions: (newDrawerOptions: IDrawerOption[]) => void;
 }
 
 const DrawerContext = createContext({} as IDrawerContextData);
@@ -18,14 +25,19 @@ interface IDrawerProviderProps {
 
 export const DrawerProvider: React.FC<IDrawerProviderProps> = ({ children }) => {
 
+  const [drawerOptions, setDrawerOptions] = useState<IDrawerOption[]>([]);
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   const toggleDrawerOpen = useCallback(() => {
     setIsDrawerOpen(oldDrawerOpen => !oldDrawerOpen);
   }, []);
 
+  const handleSetDrawerOptions = useCallback(( newDrawerOptions: IDrawerOption[] ) => {
+    setDrawerOptions(newDrawerOptions);
+  }, []);
+
   return (
-    <DrawerContext.Provider value={{ isDrawerOpen, toggleDrawerOpen }}>
+    <DrawerContext.Provider value={{ isDrawerOpen, drawerOptions, toggleDrawerOpen, setDrawerOptions: handleSetDrawerOptions }}>
       {children}
     </DrawerContext.Provider>
   );
